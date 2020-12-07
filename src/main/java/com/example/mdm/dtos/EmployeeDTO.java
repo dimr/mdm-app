565154c24d43ac58cf6b9d26e1104d0aca58f469
Dimitris Rongotis <dimitris.rongotis@gmail.com>
2020-12-07 19:50:09 +0200

    EmkployeeDTO

---
+++ src/main/java/com/example/mdm/dtos/EmployeeDTO.java
@@ -0,0 +1,52 @@
+package com.example.mdm.dtos;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmployeeDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private CompanyDTO company;
+    private String companyName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public CompanyDTO getCompany() {
+        return company;
+    }
+
+    public void setCompany(CompanyDTO company) {
+        this.company = company;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+}
